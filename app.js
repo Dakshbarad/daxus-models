09897727f9f053ba2cc4875547962412924a73c4
Dakshbarad <daksh16121999@gmail.com>
2021-04-01 11:24:04 +0530

    Changed port from 3000 to process.env.port

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require("express");
-const app= express();
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.set("view engine",'ejs');
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // Routes
-app.get("/",(req,res) =>{
-    res.render("home");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("/models",(req,res) =>{
-    res.render("models");
+app.get("/models", (req, res) => {
+  res.render("models");
 });
 
-app.get("/about",(req,res) =>{
-    res.render("about");
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 // app.get("/apply",(req,res) =>{
 //     res.render("apply");
 // });
 
-app.get("/contact",(req,res) =>{
-    res.render("contact");
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 // Port listener
-app.listen(3000,()=>{
-    console.log("server running on port 3000...");
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
