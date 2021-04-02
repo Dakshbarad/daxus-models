0518754511b76972c8af48ffcc58531659e78d24
Dakshbarad <daksh16121999@gmail.com>
2021-04-02 14:46:33 +0530

    Created the contact me page

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+// const livereload = require('livereload')
+// const connectLivereload = require('connect-livereload')
+
+// const liveReloadServer = livereload.createServer();
+// liveReloadServer.watch([__dirname+'/views',__dirname+'/public'])
+
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+// app.use(connectLivereload());
+
+// liveReloadServer.server.once("connection", () => {
+//   setTimeout(() => {
+//     liveReloadServer.refresh("/");
+//   }, 100);
+// });
 
 // Routes
 app.get("/", (req, res) => {
